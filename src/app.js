b6550e4c1f0aa387105de042171a2a5c2e992ca4
Mondo <1armandosx@gmail.com>
2023-11-05 22:05:49 -0500

    Updates port number

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.get('/api/metrics', async (req, res) => {
 });
 
 // Start Express server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
